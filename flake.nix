1d51fe11e89d5bdf9dde3dc3fd06bd2afa950ece
iancleary <github@iancleary.me>
2024-03-26 21:42:49 -0700

    Add hello to test as input

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "iancleary's Hyprland config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    systems.url = "github:nix-systems/default-linux";
 
     hyprland.url = "github:hyprwm/Hyprland";
 
@@ -13,58 +14,28 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      specialArgs = { inherit inputs; };
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    systems,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    eachSystem = lib.genAttrs (import systems);
+    pkgsFor = eachSystem (system:
+      import nixpkgs {
+        localSystem = system;
+      });
+  in {
+    packages = eachSystem (system: {
+      default = self.packages.${system}.hyprland;
+      inherit
+        (pkgsFor.${system})
+        
+        hello
+        ;
+    });
 
-      x86-system = "x86_64-linux";
-      x86-pkgs = import nixpkgs {
-        system = x86-system;
-        config = {
-          allowUnfree = true;
-        };
-      };
-
-      hyprland-desktop-modules = [
-        ./modules/desktop # folder
-        ./modules/desktop/hyprland # folder
-      ];
-
-    in
-    {
-      nixosConfigurations = {
-        framework = nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          system = x86-system;
-          # pkgs = x86-pkgs;
-          modules = common-modules ++ hyprland-desktop-modules ++ personal-modules
-            ++ [
-            ./hardware-configuration.nix # hardware-configuration/framework.nix
-            ./configuration.nix # hosts/framework.nix
-            ./home/iancleary-hyprland.nix
-          ];
-        };
-
-        vm-iancleary-nixos = nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          system = x86-system;
-          modules = common-modules ++ gnome-desktop-modules ++ virtualbox-guest-modules
-            ++ [
-            ./hardware-configuration.nix # hardware-configuration/vm-iancleary-nixos.nix
-            ./configuration.nix # hosts/vm-iancleary-nixos.nix
-            ./home/iancleary-gnome.nix
-          ];
-        };
-        vm-icleary-nixos = nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          system = x86-system;
-          modules = common-modules ++ xfce-desktop-modules ++ virtualbox-guest-modules
-            ++ [
-            ./hardware-configuration.nix # hardware-configuration/vm-icleary-nixos.nix
-            ./configuration.nix # hosts/vm-icleary-nixos.nix
-            ./home/icleary-xfce.nix
-          ];
-        };
-      };
-    };
+    # nixosModules.default = import ./hyprland.nix inputs;
+  };
 }
