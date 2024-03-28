b7552ad83164e882bb6823a2a5305e12a834849c
iancleary <github@iancleary.me>
2024-03-28 16:47:13 -0700

    Update flake.nix to be hello world

--- flake.nix
+++ flake.nix
@@ -1,40 +1,31 @@
 {
-  description = "iancleary's Hyprland config";
+  description = "iancleary's example config";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     
-    systems.url = "github:nix-systems/default-linux";
+    # systems.url = "github:nix-systems/default-linux";
 
-    hyprland.url = "github:hyprwm/Hyprland";
+    # hyprland.url = "github:hyprwm/Hyprland";
 
-    hyprland-contrib = {
-      url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # hyprland-contrib = {
+    #   url = "github:hyprwm/contrib";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
-    systems,
     ...
-  }: let
-    inherit (nixpkgs) lib;
-    eachSystem = lib.genAttrs (import systems);
-    pkgsFor = eachSystem (system:
-      import nixpkgs {
-        localSystem = system;
-      });
-  in {
-    packages = eachSystem (system: {
-      default = self.packages.${system}.hyprland;
-      inherit
-        (pkgsFor.${system})
-        
-        hello
-        ;
-    });
+  }: 
+  {
+    nixosModules.default = { pkgs, ... }:
+      {
+        environment.systemPackages = with pkgs; [
+          hello
+        ];
+      };
 
     # nixosModules.default = import ./hyprland.nix inputs;
   };
